873b0c5ec15092aa57c1a3b063e221089de52dbc
Sheikh Uzair Raza <sheikhuzairraza834@gmail.com>
2025-01-13 00:14:17 +0500

    Add program to convert temperature from Celsius to Fahrenheit

---
+++ Lab-1/temperature_conversion.py
@@ -0,0 +1,8 @@
+# Program to convert temperatures
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = celsius_to_fahrenheit(celsius)
+
+print(f"Temperature in Fahrenheit is: {fahrenheit}")
