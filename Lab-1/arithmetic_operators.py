db41ae52fcd52485bb43cbf2047e0d195a0ad409
Sheikh Uzair Raza <sheikhuzairraza834@gmail.com>
2025-01-12 23:52:33 +0500

    Add examples of arithmetic and boolean operators in Python

---
+++ Lab-1/arithmetic_operators.py
@@ -0,0 +1,21 @@
+# Arithmetic and Boolean Operators in Python
+
+# Arithmetic Operators
+# Addition
+print(1 + 1)  # Output: 2
+
+# Multiplication
+print(2 * 3)  # Output: 6
+
+# Boolean Operators
+# Equality operator
+print(1 == 0)  # Output: False
+
+# NOT operator
+print(not (1 == 0))  # Output: True
+
+# AND operator
+print((2 == 2) and (2 == 3))  # Output: False
+
+# OR operator
+print((2 == 2) or (2 == 3))  # Output: True
