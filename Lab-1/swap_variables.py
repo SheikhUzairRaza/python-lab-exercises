eaaf25d2ff88d7552b6526fbdcdcf881003e59af
Sheikh Uzair Raza <sheikhuzairraza834@gmail.com>
2025-01-13 00:12:46 +0500

    Add program to swap four variables using Python’s multiple assignment

---
+++ Lab-1/swap_variables.py
@@ -0,0 +1,16 @@
+# Program to swap four variables
+a = 2
+b = 56
+c = 78
+d = 9
+
+# Before swapping
+print("Before swapping")
+print(f"a = {a}, b = {b}, c = {c}, d = {d}")
+
+# Swapping
+a, b, c, d = d, c, b, a
+
+# After swapping
+print("After swapping")
+print(f"a = {a}, b = {b}, c = {c}, d = {d}")
